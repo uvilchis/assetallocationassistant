356bced464739ad52a7d7e1f18900e06251c5894
uvilchis <uciel.vilchis7@gmail.com>
2019-10-23 22:29:36 -0400

    init commit

---
+++ src/main/java/com/assetallocationassistant/assetallocationassistant/AssetallocationassistantApplication.java
@@ -0,0 +1,13 @@
+package com.assetallocationassistant.assetallocationassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetallocationassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetallocationassistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assetallocationassistant/assetallocationassistant/AssetallocationassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assetallocationassistant.assetallocationassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetallocationassistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
